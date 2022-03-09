2eac7b8d1673a44695d156a7016a0cf0443a2a84
SHREY PAREKH <shreyparekh1199@gmail.com>
2022-03-09 10:23:24 +0530

    First

---
+++ restful-webservices/src/main/java/com/shrey/rest/restfulwebservices/helloworld/user/PostRepository.java
@@ -0,0 +1,11 @@
+package com.shrey.rest.restfulwebservices.helloworld.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer>{
+	
+	
+
+}
